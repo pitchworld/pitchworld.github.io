f01c1fea0e2ada34b30cc44fe5e190be5c744024
pitchworld <36471835+pitchworld@users.noreply.github.com>
2021-03-06 17:15:41 +0530

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,15 +1,22 @@
 <?php
 
+$name =$_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];  
 
-$visitor_email = $_POST['email'];
+if(empty($email) )
+{
+    echo "<script type ='text/javascript'>
+    alert('plea fill');
+    window.history.go(-1);
+    </script>";
+}
+else
 
-$email_from  = 'bmathan777@gmail.com';
-$email_subject="new form ";
-$email_body="user_email : $visitor_email.\n";
-
-$to = "softwarejobsc@gmail.com";
-$headers = "from : $email_from \r\n";
-$headers = "reply-to: $visitor_email \r\n";
-mail($to_$email_subject,$email_body,$headers);
-header("location : index.html";)
-?>
+{
+    mail("bmathan777@gmail.com" , " job alert" , $message , "from : $name<$email>");
+echo "<script type ='text/javascript'>
+alert('Thank you');
+window.history.go(-1);
+</script>";
+}
